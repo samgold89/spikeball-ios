c5186a3590d4102fd0136a04dcfb5eab234d4f6a
Sam Goldstein <sammy@gainfitness.com>
2014-11-22 15:45:56 -0800

    Project setup

---
+++ Spikeball/Spikeball/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Spikeball
+//
+//  Created by Sam Goldstein on 11/22/14.
+//  Copyright (c) 2014 Sam Goldstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Spikeball/Spikeball/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spikeball
+//
+//  Created by Sam Goldstein on 11/22/14.
+//  Copyright (c) 2014 Sam Goldstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
